90e95ed25117070573f0dc0f217a3eee8e1e1092
caipora-cs <0xaoi@protonmail.com>
2023-11-27 15:59:34 +0000

    Experimenting with colors

--- lib/constant.dart
+++ lib/constant.dart
@@ -1,8 +1,8 @@
 import 'package:flutter/material.dart';
 
 // Colors
-const backgroundColor = Color(0xFFa3e4d7);
-const primaryColor = Color(0xFFe4a3b0);
-const secondaryColor = Color(0xFF768989);
+const backgroundColor = Color(0xFFd6dcd2);
+const primaryColor = Color(0xFF968ab5);
+const secondaryColor = Color(0xFF7d8293);
 
 
