84a25e1ac6e81ed397a1c46213ea205c6f164ac1
caipora-cs <0xaoi@protonmail.com>
2023-12-08 16:48:24 +0000

    Added Icons and funcionality to the HomeScreen.

--- lib/constant.dart
+++ lib/constant.dart
@@ -4,6 +4,15 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:solarpunk_prototype/controllers/authentication.dart';
 
+// Pages
+const pages = [
+  Text('Home'),
+  Text('Search'),
+  Text('Profile'),
+  Text('Messages'),
+  Text('Add'),
+];
+
 // Colors
 const backgroundColor = Color(0xFFd6dcd2);
 const primaryColor = Color(0xFF968ab5);
--- lib/views/screens/home_view.dart
+++ lib/views/screens/home_view.dart
@@ -1,15 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:solarpunk_prototype/views/widgets/punk_icon.dart';
+import '../../constant.dart';
 
-class HomeScreen extends StatelessWidget {
+class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
 
   @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int pageIndex = 0;
+
+  @override
   Widget build(BuildContext context) {
-   return Scaffold(
-     body: Container(
-       alignment: Alignment.center,
-       child: Text('Home Screen'),
-     ),
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (index) {
+          setState(() {
+            pageIndex = index;
+          });
+        },
+        type: BottomNavigationBarType.fixed,
+        backgroundColor: backgroundColor,
+        selectedItemColor: primaryColor,
+        unselectedItemColor: secondaryColor,
+        currentIndex: pageIndex,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home, size: 30),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.search, size: 30),
+            label: 'Search',
+          ),
+          BottomNavigationBarItem(
+            icon: PunkIcon(),
+            label: '',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.message, size: 30),
+            label: 'Messages',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person, size: 30),
+            label: 'Profile',
+          ),
+        ],
+      ),
+      body: pages[pageIndex],
     );
   }
 }
